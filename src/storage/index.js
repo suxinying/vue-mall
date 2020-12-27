8e757656f9e2d903bf4757deefae4e2ce10c1e7c
suxinying <34708049+suxinying@users.noreply.github.com>
2020-12-27 23:57:04 +0800

    sessionStorage封装

---
+++ src/storage/index.js
@@ -0,0 +1,35 @@
+const STORAGE_KEY = 'mall';
+export default {
+    setItem(key, value, module_name) {
+        if (module_name) {
+            let val = this.getItem(module_name);
+            val[key] = value;
+            this.setItem(module_name, val)
+        } else {
+            let mallObj = this.getStorage();
+            mallObj[key] = value;
+            window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(mallObj))
+        }
+    },
+    getItem(key, module_name) {
+        if (module_name) {
+            let moduleObj = this.getItem(module_name)
+            if (moduleObj) {
+                return moduleObj[key]
+            }
+        }
+        return this.getStorage()[key]
+    },
+    getStorage() {
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '{}')
+    },
+    clear(key, module_name) {
+        let mallObj = this.getStorage();
+        if(module_name){
+            delete mallObj[module_name][key];
+        }else {
+            delete mallObj[key];
+        }
+        this.setItem(mallObj);
+    }
+}
